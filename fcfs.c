afe0d752c5531def5257f154455bdd84307da167
Prahashit4 <85918091+Prahashit4@users.noreply.github.com>
2021-11-16 11:45:14 +0530

    Add files via upload

---
+++ fcfs.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+struct fcfs
+{
+	int at,st,str,ft,tat,wt;
+}p[50];
+main()
+{
+	int i,j,n;
+	float atrt=0,awt=0;
+	printf("\n enter the no of processes:");
+	scanf("%d",&n);
+	printf("\n enter the arrival times of the processes:");
+	for(i=0;i<n;i++)
+	scanf("%d",&p[i].at);
+	printf("\n Enter the service times of the processes:");
+	for(i=0;i<n;i++)
+	scanf("%d",&p[i].st);
+	p[0].str=p[0].at;
+	for(j=0;j<n;j++)
+	{
+		p[j].ft=p[j].str+p[j].st;
+		p[j+1].str=p[j].ft;
+	}
+	for(i=0;i<n;i++)
+	{
+		p[i].tat=p[i].ft-p[i].at;
+		atrt=atrt+p[i].tat;
+		p[i].wt=p[i].str-p[i].at;
+		awt=awt+p[i].wt;
+	}
+	printf("process\tAT\tST\tSTR\tFT\tTAT\tWT\n");
+	for(i=0;i<n;i++)
+	{
+		printf("p%d\t%d\t%d\t%d\t%d\t%d\t%d\n",i,p[i].at,p[i].st,p[i].str,p[i].ft,p[i].tat,p[i].wt);
+	}
+	atrt=atrt/n;
+	awt=awt/n;
+	printf("Average turn around time = %f",atrt);
+	printf("Average waiting time = %f",awt);
+}
